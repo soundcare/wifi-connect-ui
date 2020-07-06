47553a1d5ed113433a1aec4e81dac71b1638401a
romulo <romulomanzano@gmail.com>
2020-07-06 09:22:14 -0400

    Initial version

---
+++ build/precache-manifest.64113a3ad64d1a47a0fcd61ef111ee4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c21fde4360d6af08faa1a0ee08e2b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3458adb18787889b5c1f",
+    "url": "/static/js/2.080d2ba7.chunk.js"
+  },
+  {
+    "revision": "63748a42c768e6232479f8dd8348a100",
+    "url": "/static/js/2.080d2ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35294a96d9991602935",
+    "url": "/static/js/main.2eeb6cc4.chunk.js"
+  },
+  {
+    "revision": "0e903404fe668039b1a7",
+    "url": "/static/js/runtime-main.2a78626f.js"
+  }
+]);
